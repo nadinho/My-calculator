62146d913340aaf854362df49dcf695803730780
nadinho <rockt2935@gmail.com>
2020-02-26 11:13:03 +0100

    add functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,15 @@
-document.querySelector(".calculator__output").value = "12";
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = divide(33, 87);
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
