6edb118408309ffab2a5e4bc6c00ff86f86ed874
nadinho <rockt2935@gmail.com>
2020-02-26 14:29:38 +0100

    add result and clear functionality

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,7 @@
 const calculatorOutput = document.querySelector(".calculator__output");
-calculatorOutput.value = divide(33, 87);
+const calcInputs = document.querySelectorAll(".calc__input");
+const calcResult = document.querySelector("#calc__long");
+const calcClear = document.querySelector(".calc__delete");
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -13,3 +15,24 @@ function multiply(numberOne, numberTwo) {
 function divide(numberOne, numberTwo) {
   return numberOne / numberTwo;
 }
+
+// EQUAL SIGN
+const numberOne = Number(calcInputs[4].innerText);
+const numberTwo = Number(calcInputs[8].innerText);
+// calculatorOutput.value = add(numberOne, numberTwo);
+function handleClick() {
+  calculatorOutput.value = divide(numberOne, numberTwo);
+  console.log("clicked");
+}
+calcResult.addEventListener("click", handleClick);
+
+// DELETE
+
+function allClear() {
+  calculatorOutput.value = 0;
+}
+calcClear.addEventListener("click", allClear);
+
+4, 5, 6;
+8, 9, 10;
+12;
