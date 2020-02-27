2f59f461d076a8607d4e8e3a68e60946a2984f01
Nadine Gehrmann <57504815+nadinho@users.noreply.github.com>
2020-02-27 11:18:50 +0100

    Add Module (#4)
    * add math.js
    
    * export and import functions
    
    * removed unused functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,10 @@
+import { add } from "./math.js";
+
 const calculatorOutput = document.querySelector(".calculator__output");
 const calcInputs = document.querySelectorAll(".calc__input");
 const calcResult = document.querySelector("#calc__long");
 const calcClear = document.querySelector(".calc__delete");
 const calcOperators = document.querySelectorAll(".calc__operator");
-
-// MATH
-function add(numberOne, numberTwo) {
-  return numberOne + numberTwo;
-}
-function subtract(numberOne, numberTwo) {
-  return numberOne - numberTwo;
-}
-function multiply(numberOne, numberTwo) {
-  return numberOne * numberTwo;
-}
-function divide(numberOne, numberTwo) {
-  return numberOne / numberTwo;
-}
 
 let numberOne = 0;
 let numberTwo = 0;
